21ad89f577a0ad43f748e4ff9521aed5259f2238
valdirsb <valdirsb@gmail.com>
2021-07-19 22:52:28 -0300

    Primeiro Projeto com Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Tutorial/>
+
+    <NuxtLink to="/page2">link</NuxtLink>
+  </div>
+  
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/page2.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <p>Bem vindo {{nome}}</p>
+    <button class="btn btn-primary">Clique aqui</button>
+</div>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nome: 'Valdir'
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
