f1fbffeb58ade3e903fff72a3b7b9f3b6cdbeeaf
valdirsb <valdirsb@gmail.com>
2021-07-22 23:15:22 -0300

    teste

--- pages/page2.vue
+++ pages/page2.vue
@@ -1,9 +1,13 @@
 <template>
 <div>
-    <h1>Pagina 2</h1>
-    <p>Bem vindo {{nome}}</p>
-    <p>Este é o produto {{produto}}</p>
-    <button class="btn btn-primary">Clique aqui</button>
+    <h1>{{produto.nome}}</h1>
+    <p>{{produto.nomdescricao}}</p>
+    <p>{{produto.valor}}</p>
+
+    <button v-on:click="add()" class="btn btn-primary">Adicionar</button>
+    <p>Quantidade: {{qt}}</p>
+    <button v-on:click="rem()" class="btn btn-primary">retirar</button>
+    <p>Valor Total: {{valortotal}}</p>
 </div>
   
 </template>
@@ -13,8 +17,39 @@ export default {
     layout: 'tema2',
     data() {
         return {
-            nome: 'Valdir'
+            nome: 'Valdir',
+            qt:1,
+            produto: {
+                    nome:'Produto 1',
+                    descricao:'Desc produdo 1',
+                    valor: 20.00
+                },
+            valortotal: 0,
         }
+    },
+    methods: {
+        add: function(){
+            this.qt++
+        },
+        rem: function(){
+            if(this.qt > 1){
+                this.qt--
+            }
+            
+        },
+    },
+
+    watch: {
+        qt: function () {
+            this.valortotal = this.qt*this.produto.valor
+        }
+    },
+
+
+    qt: function () {
+
+        this.valortotal = this.qt*this.produto.valor
+
     }
 
 }
